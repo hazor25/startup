7c14dcddaf07c967231feb59cc629a2a7ab278d2
Luis Rosas <luisr112501@gmail.com>
2026-07-10 16:55:11 -0600

    Added folders and .jsx files to src, with respective name changes, and edited app.jsx as requested

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Play } from './play/play';
+import { Menu } from './menu/menu';
+import { Lobby } from './lobby/lobby';
+
 
 export default function App() {
   return (
-    <div className="body bg-dark text-light">
+    <BrowserRouter>
+      <div className="body bg-dark text-light">
       <header className="container-fluid">
         <nav className="navbar fixed-top navbar-dark">
           <div className="navbar-brand">
@@ -45,6 +52,8 @@ export default function App() {
           </a>
         </div>
       </footer>
-    </div>
+      </div>
+    </BrowserRouter>
   );
 }
+
---
+++ src/lobby/lobby.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Lobby() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>lobby displayed here</div>
+    </main>
+  );
+}
