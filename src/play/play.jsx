863ab8f1e124c382ccc1ea39a069c7fa97a48443
Luis Rosas <luisr112501@gmail.com>
2026-07-22 14:15:06 -0600

    Fixed user checking mismatch and imported useEffect to stop crashing in play.jsx

--- src/play/play.jsx
+++ src/play/play.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import { useNavigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
