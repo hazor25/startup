4d39f85f1179e7edd3ebdfb3cb99ecc796677de4
Luis Rosas <luisr112501@gmail.com>
2026-08-03 13:48:44 -0600

    Added uri encoding for new database password

--- service/database.js
+++ service/database.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require('mongodb');
 const config = require('./dbConfig.json');
 
-const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+const url = `mongodb+srv://${config.userName}:${encodeURIComponent(config.password)}@${config.hostname}`;
 const client = new MongoClient(url);
 const db = client.db('startup');
 const userCollection = db.collection('user');
@@ -33,11 +33,11 @@ async function updateUserToken(username, token) {
 }
 
 async function clearUserToken(token) {
-  const result = await userCollection.updateOne(
-    { token: token },
-    { $set: { token: null } }
-  );
-  return result;
+    const result = await userCollection.updateOne(
+        { token: token },
+        { $set: { token: null } }
+    );
+    return result;
 }
 
 async function saveGame(username, gameData) {
